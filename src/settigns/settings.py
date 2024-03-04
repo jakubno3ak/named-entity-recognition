d321589a05c1ec9eebf71bc3bdc3b54a4bd4c0bd
kubano <jakubno3ak@gmail.com>
2024-03-04 18:12:26 +0100

    Add settings

---
+++ src/settigns/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    model_name: str = "distilbert-finetuned-ner"
+    checkpoint: str =  "distilbert-base-uncased"
+    zip_data_full_path: str = "./data/archive.zip"
+    csv_data_dir_path: str = "./data/data_csv"
+    json_data_dir_path: str = "./data/data_json"
+    metric: str = 'seqeval'
+
+    evaluation_strategy: str = 'epoch'
+    save_strategy: str = 'epoch'
+    learning_rate: float = 3e-5
+    num_train_epochs: int = 10
+    weight_decay: float = 0.01
+    seed: int = 42
+    batch_size: int = 16
